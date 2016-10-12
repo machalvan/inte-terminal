501f75d586af1606ba19a062e625e569f6ef655a
DISKTiger <sebastianstavenius@gmail.com>
2016-10-12 16:44:59 +0200

    enIdé

---
+++ src/Project/test1.java
@@ -0,0 +1,13 @@
+package Project;
+
+public class test1 {
+
+	public static String testString = "Byggscriptet fungerar";
+	
+	
+	public static void main(String[] args) {
+		System.out.print(testString);
+
+	}
+
+}
