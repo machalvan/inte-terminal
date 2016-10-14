09730c7699b68f73948f42d099a13655bb7a7e0c
sega9727 <sebastian.gabrielsson@gmail.com>
2016-10-14 16:50:23 +0200

    Tidig version av konsol terminal

---
+++ src/commandLineIntTest.java
@@ -0,0 +1,19 @@
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//public class commandLineIntTest {
+//	
+//	commandLineInt cli = new commandLineInt();
+//	
+//	@Test
+//	public void setDirTest() {
+//		assertEquals("/tempTest",cli.setWorkingDir());
+//	}
+//	
+//	@Test
+//	public void checkDirTest(){
+//		assertEquals("/tempTest",cli.getCurrentDir());
+//	}
+//
+//}
