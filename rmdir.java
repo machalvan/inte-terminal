96134a3503e79b967d3659184fb0cfff6eb2809c
DISKTiger <sebastianstavenius@gmail.com>
2016-10-26 00:38:41 +0200

    Uppdaterad version

--- rmdir.java
+++ rmdir.java
@@ -5,28 +5,22 @@ public class rmdir {
 	public String userInput;
 	
 	public static Boolean removeDirectory(File directory) {
-
-		//File folder = new File(System.getProperty("user.dir"));
-		
-		
-		/*
-		 * error ifall katalogen inte är tom
-		 * error ifall filen inte hittas
-		 */
 		
 		try {
-			if(directory.exists()){
-				if(directory.isDirectory()){
+			if(directory.exists()) {
+				if(directory.isDirectory()) {
 					File[] files = directory.listFiles();
-					for(File file : files){
-						file.delete();
-					}
-					
-					//folder.delete();
-					
+					for(File file : files) {
+						if(directory.isDirectory()) {
+							removeDirectory(file);
+						}
+						else {
+							file.delete();
+						}
+					}	
 				}
 			}
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return (directory.delete());
@@ -34,30 +28,10 @@ public class rmdir {
 	}
 	
 	
-//	
-//	public static Boolean removeDirectoryOne(File directory) {
-//	    if(directory.exists()){
-//	    	File[] files = directory.listFiles();
-//	    	if(files!=null){
-//	    		for(int i=0; i<files.length; i++) {
-//	    			if(files[i].isDirectory()) {
-//	    				removeDirectory(files[i]);
-//	    			}
-//	    			else {
-//	    				files[i].delete();
-//	    			}
-//	    		}
-//	    	}
-//	    }
-//		return(directory.delete());	
+//	public static void main(String[] args) {
+//		File folder = new File("D:\\Workspace\\InteProject\\tempTest");
+//		removeDirectory(folder);
 //	}
-//	
-	
-	
-	public static void main(String[] args){
-		File folder = new File("D:\\Workspace\\InteProject\\tempTest");
-		removeDirectory(folder);
-	}
 	
 	public String setUserInput(String ui) {
 		userInput = ui;
@@ -67,4 +41,14 @@ public class rmdir {
 	public String getUserInput() {
 		return userInput;
 	}
+	
+	public boolean checkUi(String ui) {
+		if(ui == null) {
+			return false;
+		} 
+		else {
+			return true;
+		}
+	}
+	
 }
