949152f2a7b94e87277b7020f1ca28a676b56d5b
DISKTiger <sebastian.stavenius@gmail.com>
2019-06-18 20:38:25 +0200

    Added code for cd .. command and checked that all tests passed

--- src/main/java/commands/Cd.java
+++ src/main/java/commands/Cd.java
@@ -1,6 +1,7 @@
 package commands;
 
 import java.io.File;
+import java.util.Arrays;
 
 
 public class Cd extends Command {
@@ -17,6 +18,12 @@ public class Cd extends Command {
 		if (!file.exists()) {
 			System.out.println("No such directory exists");
 			return workingDir;
+		}  else if (data.equalsIgnoreCase("..")) {
+			String[] dir = workingDir.getAbsolutePath().split("\\\\");
+			String[] newDir = Arrays.copyOf(dir, dir.length - 1);
+			String newWorkingDir = String.join("\\", newDir);
+			File newFile = new File(newWorkingDir);
+			return newFile;
 		} else {
 			return file;
 		}
